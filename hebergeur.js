24cabc1c7f924f1fb2f7cb2f6104ba0839eeb3f1
ScheanH <95644537+ScheanH@users.noreply.github.com>
2022-02-01 14:04:02 +0100

    Create hebergeur.js

---
+++ hebergeur.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+const { prefix } = require('../db/config.json')
+
+module.exports.run = async(bot, message, args) => {
+
+let HebergeurEmbed = new Discord.MessageEmbed()
+.setThumbnail(bot.user.displayAvatarURL({dynamic: true, size: 512}))
+.setColor('00ff26')
+.setDescription('Permet de savoir l\'état de l\'hébergeur du bot')
+.setTitle('Hébergement')
+.addField('L\'état de votre Hébergeur est : \n\`ACTIF\`')
+}
+
+module.exports.config = {
+    name: "hebergeur"
+}
